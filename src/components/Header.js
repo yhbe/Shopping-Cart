754e9c9cc7dc22f2fcb3c6a34e89dffe984a332f
SHEF <Shefqetmush@gmail.com>
2023-01-27 22:29:40 -0500

    header component made and styled

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+export default function Header(){
+  return (
+    <div className="header--container">
+      <h1 className="header--container-title">FakeStore</h1>
+      <ul className="header--items">
+        <li>Home</li>
+        <li>Products</li>
+        <li>Contact</li>
+        <li className="header--items-shopping-cart-li">
+          <i class="fa-solid fa-cart-shopping"></i>
+        </li>
+      </ul>
+    </div>
+  );
+}
