d8f08b7118d38ba0986ebd7132c9b5f6fb603c3e
SHEF <Shefqetmush@gmail.com>
2023-01-30 16:36:08 -0500

    contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,8 +3,25 @@ import Header from "../components/Header";
 export default function Contact () {
   return (
     <>
-    <Header></Header>
-    <h1>Hello Contact</h1>
+      <Header></Header>
+      <ul className="contact--icons-ul">
+        <li>
+          <i class="fa-brands fa-github">
+          </i>
+        </li>
+        <li>
+          <i class="fa-brands fa-facebook"></i>
+        </li>
+        <li>
+          <i class="fa-brands fa-facebook-messenger"></i>
+        </li>
+        <li>
+          <i class="fa-brands fa-twitter"></i>
+        </li>
+        <li>
+          <i class="fa-brands fa-instagram"></i>
+        </li>
+      </ul>
     </>
-  )
+  );
 }
